04b0a040897c1ca2cab51c3b48d934ff8e4a5115
Lee, yeeun <ye7426@naver.com>
2024-09-25 07:26:51 +0900

    Hello클래스 수

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,19 +1,20 @@
 package git_test2;
 
 public class Hello {
-<<<<<<< HEAD
-    
-    String message = "안녕하세요";
+
+    String message;
+
+    public Hello() {
+        this("안녕하세요");
+    }
+
+    public Hello(String message) {
+        this.message = message;
+    }
 
     public void say() {
         System.out.println(message);
     }
-=======
-	
-	public void say() {
-		System.out.println("안녕하세요");
-	}
->>>>>>> c8deb18 (main 메소드 수정)
 
 }
 
