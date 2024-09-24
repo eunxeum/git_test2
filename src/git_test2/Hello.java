34bf6f3b4db9cd921f0b6e8205a449486aa37d7c
Lee, yeeun <ye7426@naver.com>
2024-09-25 07:12:51 +0900

    main method modify

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,12 +1,19 @@
 package git_test2;
 
 public class Hello {
+<<<<<<< HEAD
     
     String message = "안녕하세요";
 
     public void say() {
         System.out.println(message);
     }
+=======
+	
+	public void say() {
+		System.out.println("안녕하세요");
+	}
+>>>>>>> c8deb18 (main 메소드 수정)
 
 }
 
